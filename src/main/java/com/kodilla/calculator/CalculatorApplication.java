ded0735de7d8700c42ba7bba3358da22780b62bc
rafalpasiut <rafalpasiut@gmail.com>
2017-12-08 20:35:40 +0100

    calculator html css view

--- src/main/java/com/kodilla/calculator/CalculatorApplication.java
+++ src/main/java/com/kodilla/calculator/CalculatorApplication.java
@@ -3,10 +3,12 @@ package com.kodilla.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
+
 }
